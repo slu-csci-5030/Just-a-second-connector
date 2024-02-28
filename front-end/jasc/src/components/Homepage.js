e9d095d5a016822fec7d82c0ca864aab28e30356
snandakumar2912 <144180105+snandakumar2912@users.noreply.github.com>
2024-02-27 19:05:32 -0600

    Creating a Homepage

---
+++ front-end/jasc/src/components/Homepage.js
@@ -0,0 +1,15 @@
+// Homepage.js
+import React from 'react';
+import '../styles/Homepage.css'; // Adjust the import path
+
+function Homepage() {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1 className="title">Just a Second Connector</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Homepage;
