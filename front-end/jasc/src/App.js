a55de8e1075ad39aa5a8d9d26a8434cf529571fd
Sai Joshika Gummireddy <saijoshikareddy@gmail.com>
2024-02-28 02:02:48 -0600

    Commit Homepage button

---
+++ front-end/jasc/src/App.js
@@ -0,0 +1,21 @@
+// App.js
+import React from "react";
+
+import Homepage from "./components/Homepage";
+import "./App.css";
+
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <Homepage />
+        </header>
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
