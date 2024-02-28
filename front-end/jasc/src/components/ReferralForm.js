8b53a241f8a02a70b1fd595d1b3d51b2407c020a
MutyaSowmya123 <156967848+MutyaSowmya123@users.noreply.github.com>
2024-02-28 01:45:11 -0600

    ADDED ReferralForm page

---
+++ front-end/jasc/src/components/ReferralForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/ReferralForm.css';
+
+function  ReferralForm(e) {
+    return(
+        
+            <h1>Referral Form</h1>
+       
+    );
+}
+
+export default ReferralForm;
