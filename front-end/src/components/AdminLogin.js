287f209b062f4db81ff7e32785e48bea3cf9238f
Harsh Patel <149712691+Harvey1314@users.noreply.github.com>
2024-03-26 18:46:59 -0500

    Added Admin button to navbar

---
+++ front-end/src/components/AdminLogin.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom'; // Import useHistory hook for navigation
+import '../styles/AdminLogin.css';
+
+const AdminLogin = () => {
+    return (
+        <div className="admin-login-container">
+            <h2>Admin Login</h2>
+        </div>
+    );
+};
+
+export default AdminLogin;
