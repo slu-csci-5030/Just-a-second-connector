5d6a891b67268d9790c2d6c01a890d7d341b44ed
MutyaSowmya123 <156967848+MutyaSowmya123@users.noreply.github.com>
2024-03-26 09:24:02 -0500

    commits

--- front-end/src/components/AdminDashBoard.js
+++ front-end/src/components/AdminDashBoard.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // AdminDashboard.js
 
 import React from 'react';
@@ -13,3 +14,20 @@ const AdminDashboard = () => {
 
 export default AdminDashboard;
 
+=======
+// AdminDashboard.js
+
+import React from 'react';
+import '../styles/AdminDashBoard.css';
+
+const AdminDashboard = () => {
+    return (
+        <div className="admin-dashboard-container">
+            <h2 className="dashboard-heading">Welcome to the Admin Dashboard!</h2>            
+        </div>
+    );
+};
+
+export default AdminDashboard;
+
+>>>>>>> Stashed changes
