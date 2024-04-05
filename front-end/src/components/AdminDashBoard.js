2b511a113cdb10b3e8d6d5024523226d98c7fdaf
snandakumar2912 <144180105+snandakumar2912@users.noreply.github.com>
2024-04-04 21:39:29 -0500

    Added navbar to Admin Dashboard

---
+++ front-end/src/components/AdminDashBoard.js
@@ -0,0 +1,24 @@
+// AdminDashboard.js
+
+import React from 'react';
+import '../styles/AdminDashBoard.css'; // Make sure to include your CSS styles
+
+const AdminDashboard = () => {
+    return (
+        <div className="admin-dashboard-container">
+            {/* Navbar Section */}
+            <h2 className="dashboard-heading">Welcome to the Admin Dashboard!</h2>
+            <div className="navbar">
+                <div className="navbar-left">
+                    <div className="content">Match job seekers with job coaches</div>
+                    <div className="content">Match Jobseekers with Employers</div>
+                </div>
+            </div>
+
+            {/* Main Dashboard Content */}
+            
+        </div>
+    );
+};
+
+export default AdminDashboard;
